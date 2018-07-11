6eb219e0ec84215fb291ef94f624ed5706c3b01a
griooo <gri@bk.ru>
2018-07-11 23:19:15 +0300

    list of map

--- src/main/java/gr/merger/MapUtil.java
+++ src/main/java/gr/merger/MapUtil.java
@@ -1,10 +1,17 @@
 package gr.merger;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.BinaryOperator;
 
 public class MapUtil {
+
+	public static <K, V> Map<K, V> mergeMaps(List<Map<K, V>> maps, BinaryOperator<V> combiner) {
+		return maps.stream().parallel().reduce((m1, m2) -> merge(m1, m2, combiner)).orElse(Collections.emptyMap());
+
+	}
 
 	/**
 	 * @param map1
--- src/test/java/gr/merger/MapUtilTest.java
+++ src/test/java/gr/merger/MapUtilTest.java
@@ -3,7 +3,9 @@ package gr.merger;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MapUtilTest {
@@ -38,9 +40,38 @@ public class MapUtilTest {
 	}
 
 	@Test
+	public void repeater() {
+		for (int i = 0; i < 100000; i++) {
+			listOfMapTest();
+		}
+	}
+
+	@Test
+	public void listOfMapTest() {
+		List<Map<String, Integer>> maps = Arrays.asList(
+				getFilledMap(2),
+				getFilledMap(3),
+				getFilledMap(4),
+				getFilledMap(5),
+				getFilledMap(6));
+
+//		System.out.println(maps);
+		Map<String, Integer> result = MapUtil.mergeMaps(maps, Integer::sum);
+//		System.out.println(result);
+		Assert.assertEquals(0, (int) result.get("k0"));
+		Assert.assertEquals(5, (int) result.get("k1"));
+		Assert.assertEquals(8, (int) result.get("k2"));
+		Assert.assertEquals(9, (int) result.get("k3"));
+		Assert.assertEquals(8, (int) result.get("k4"));
+		Assert.assertEquals(5, (int) result.get("k5"));
+
+	}
+
+
+	@Test
 	public void bigMapTest() {
-		Map<String, Integer> map1 = getRandom(3000000);
-		Map<String, Integer> map2 = getRandom(4);
+		Map<String, Integer> map1 = getFilledMap(3000000);
+		Map<String, Integer> map2 = getFilledMap(4);
 		long tStart = System.currentTimeMillis();
 		Map<String, Integer> summarized = null;
 		for (int i = 0; i < 100; i++) {
@@ -50,7 +81,7 @@ public class MapUtilTest {
 		System.out.println(tEnd - tStart + " ms " + summarized.size());
 	}
 
-	private Map<String, Integer> getRandom(int size) {
+	private Map<String, Integer> getFilledMap(int size) {
 		HashMap<String, Integer> map = new HashMap<>(size);
 		for (int i = 0; i < size; i++) {
 			map.put("k" + i, i);
