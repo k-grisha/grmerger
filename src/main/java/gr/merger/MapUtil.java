921267a6b810bdab6c0ff01865484c7cd2c8f940
Kiladze <kiladze@vbankcenter.ru>
2018-07-13 16:53:42 +0300

    тесты

--- src/main/java/gr/merger/MapUtil.java
+++ src/main/java/gr/merger/MapUtil.java
@@ -1,6 +1,6 @@
 package gr.merger;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,19 +11,33 @@ import java.util.stream.Collector;
 public class MapUtil {
 
 	public static <K, V> Map<K, V> mergeMaps(List<Map<K, V>> maps, BinaryOperator<V> combiner) {
-//		return maps.stream().parallel().reduce((m1, m2) -> merge(m1, m2, combiner)).orElse(Collections.emptyMap());
-		final Map<K, V> accumulationMap = new ConcurrentHashMap<>();
+//		return maps.stream().parallel().reduce( (m1, m2) -> merge(m1, m2, combiner)).orElse(Collections.emptyMap());
 
-		Collector<Map<K, V>, Map<K, V>, Map<K, V>> collector = Collector.of(() -> accumulationMap
-				, (map, element) -> merge(map, element, combiner)    //accumulator
+		final Map<K, V> accumulationMap = new ConcurrentHashMap<>();
+		Collector<Map<K, V>, Map<K, V>, Map<K, V>> collector = Collector.of(
+				() -> accumulationMap
+				, (Map<K, V> map, Map<K, V> element) -> map.putAll(merge(map, element, combiner))    //accumulator
 				, ((right, left) -> accumulationMap)
 				, Collector.Characteristics.CONCURRENT, Collector.Characteristics.UNORDERED);
-
-		Map<K, V> asd = maps.stream()
+		Map<K, V> asd = maps.parallelStream()
 				.collect(collector);
+
+//		final Map<K, V> accumulationMap = new ConcurrentHashMap<>();
+//		Map<K, V> asd = maps.parallelStream().collect(
+//				() -> accumulationMap,
+//				(Map<K, V> accumulator, Map<K, V> element) -> accumulator.putAll(merge(accumulator, element, combiner)),
+//				(m1, m2) -> merge(m1, m2, combiner)
+//		);
 
 		return asd;
 		//.orElse(Collections.emptyMap());
+
+	}
+
+	private static <K, V> void accumulate(Map<K, V> acuumulator, Map<K, V> map,
+										  BinaryOperator<V> combiner) {
+
+		acuumulator.putAll(merge(acuumulator, map, combiner));
 
 	}
 
@@ -37,18 +51,20 @@ public class MapUtil {
 	 */
 	public static <K, V> Map<K, V> merge(Map<K, V> map1, Map<K, V> map2,
 										 BinaryOperator<V> combiner) {
+
 		Map<K, V> bigMap;
 		Map<K, V> smallMap;
 		if (map1.size() > map2.size()) {
-			bigMap = map1;
-			smallMap = map2;
+			bigMap = new ConcurrentHashMap<>(map1) ;
+			smallMap = new ConcurrentHashMap<>(map2);
 		} else {
-			bigMap = map2;
-			smallMap = map1;
+			bigMap = new ConcurrentHashMap<>(map2) ;
+			smallMap = new ConcurrentHashMap<>(map1) ;
 		}
 
-		Map<K, V> resultMap = new HashMap<>(bigMap);
+		Map<K, V> resultMap = new ConcurrentHashMap<>(bigMap);
 		smallMap.forEach((k, v) -> resultMap.merge(k, v, combiner));
+		System.out.println(Thread.currentThread().getName());
 		return resultMap;
 	}
 }
